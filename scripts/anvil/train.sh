eeb71fbf86f70d93f1a0971c15c40eabe33eb84c
enyoukai <anjo0@cs.washington.edu>
2025-09-13 20:32:27 -0400

    fixing train batch

--- scripts/anvil/train.sh
+++ scripts/anvil/train.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 #SBATCH --partition=ai
-#SBATCH --account=ai250124-ai
-#SBATCH --mem-per-gpu=128G
-#SBATCH --cpus-per-gpu=20
+#SBATCH --account=nairr250124-ai
+#SBATCH --mem-per-gpu=96G
+#SBATCH --cpus-per-gpu=1
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
-#SBATCH --time=3:59:00
+#SBATCH --time=1:59:00
 #SBATCH --job-name=train
-#SBATCH --output=/anvil/projects/ai250124/x-pkeung/anjo0/taudio/scripts/%x/%j.out
-#SBATCH --error=/anvil/projects/ai250124/x-pkeung/anjo0/taudio/scripts/%x/%j.err
+#SBATCH --output=/anvil/scratch/x-pkeung/taudio/scripts/logs/%x/%j.out
+#SBATCH --error=/anvil/scratch/x-pkeung/taudio/scripts/logs/%x/%j.err
 
-cd /anvil/projects/ai250124/x-pkeung/anjo0/taudio
+cd /anvil/scratch/x-pkeung/taudio
 module load conda
 conda activate ./env
+
 python train.py --config $1
