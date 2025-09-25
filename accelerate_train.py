Fixing causal mask

accelerate_train.py
@@ -129,7 +129,8 @@ def main():
         drop_last=True,
         pin_memory=True,
         num_workers=8,
-        collate_fn=collate_fn
+        collate_fn=collate_fn,
+        shuffle=True
     )
 
     optim = torch.optim.AdamW(model.parameters(), lr=training_config['learning_rate'])