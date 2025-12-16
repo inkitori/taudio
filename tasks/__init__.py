d1fe38d977723090db3a3285597e4f22cb593987
inkitori <an.josephzero@gmail.com>
2025-12-16 00:17:16 -0500

    Removing Timestamp Single task

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,13 +1,11 @@
 from typing import Dict, Type
 
 from .base_task import BaseTask
-from .timestamp_single import SingleTimestampTask
 from .speaker_count import SpeakerCountTask
 from .timestamp_all import AllTimestampsTask
 from .timestamp_single_any import SingleTimestampAnyTask
 
 _TASKS: Dict[str, Type[BaseTask]] = {
-    "SINGLE_WORD_TIMESTAMP": SingleTimestampTask,
     "SINGLE_WORD_TIMESTAMP_ANY": SingleTimestampAnyTask,
     "ALL_TIMESTAMPS": AllTimestampsTask,
     "SPEAKER_COUNT": SpeakerCountTask,
