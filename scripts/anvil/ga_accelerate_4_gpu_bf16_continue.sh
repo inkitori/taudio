754cbe75de5364c45c2d2dc6f233be25ba387a1c
inkitori <an.josephzero@gmail.com>
2025-12-26 01:39:00 -0500

    Adding ga_accelerate_4_gpu_bf16_continue.sh for continuing training runs with multi timestamp training

---
+++ scripts/anvil/ga_accelerate_4_gpu_bf16_continue.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --partition=ai
+#SBATCH --account=nairr250124-ai
+#SBATCH --mem-per-gpu=96G
+#SBATCH --cpus-per-gpu=1
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:4
+#SBATCH --time=5:00:00
+#SBATCH --job-name=4_gpu_bf16_continue
+#SBATCH --output=scripts/anvil/logs/%x/%j.out
+#SBATCH --error=scripts/anvil/logs/%x/%j.err
+
+export OMP_NUM_THREADS=$(lscpu -b -p=CPU | grep -v '^#' | wc -l)
+
+master_addr=$(scontrol show hostnames "$SLURM_JOB_NODELIST" | head -n 1)
+export MASTER_ADDR=$master_addr
+echo "MASTER_ADDR: $MASTER_ADDR"
+
+export MASTER_PORT=$(expr 10000 + $(echo -n $SLURM_JOBID | tail -c 4))
+echo "MASTER_PORT: $MASTER_PORT"
+
+module load conda
+conda activate ./env
+
+accelerate launch --config_file accelerate_configs/4_gpu_bf16.yaml ga_run.py --config "$1" --load-checkpoint "$2" --run-id "$3"
