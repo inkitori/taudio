43b3d5d45ac281616a34997c96fa56c013ba3312
inkitori <an.joseph0001@gmail.com>
2025-12-28 08:03:11 -0500

    Updating runs.sh to include bernoulli test sweeps

--- runs.sh
+++ runs.sh
@@ -190,3 +190,44 @@ sbatch $EXCLUDE_NODES scripts/anvil/accelerate_1_gpu_bf16_eval.sh configs/qwen3b
 # WE ALREADY HAVE <4, <8, <12, <20 for poisson
 # Poisson <16, best 20ms
 sbatch $EXCLUDE_NODES scripts/anvil/accelerate_1_gpu_bf16_eval.sh configs/qwen3b/librispeech/timestamp_any/ablation/poisson+bidirectional_audio[start][bias_-6][bf16][max_16].yaml outputs/qwen3b/librispeech/timestamp_any/ablation/poisson+bidirectional_audio[start][bias_-6][bf16][max_16]/20251224_222331/checkpoint_epoch3 test 16 20
+
+
+# ----------- BERNOULLI TEST SWEEP -----------
+
+# THESE ARE BASED ON THE TECHNICALLY BROKEN INFERENCE STRATEGIES
+
+# 3B
+# Librispeech
+sbatch $EXCLUDE_NODES scripts/anvil/accelerate_1_gpu_bf16_eval.sh configs/qwen3b/librispeech/timestamp_any/new_inference/bernoulli[class_weighting][start][bf16][upscale_4][epoch_3][no_schedule][lr_1e-6].yaml outputs/qwen3b/librispeech/timestamp_any/new_inference/bernoulli[class_weighting][start][bf16][upscale_4][epoch_3][no_schedule][lr_1e-6]/20251221_235944/checkpoint_epoch3 test
+
+# Libricount
+sbatch $EXCLUDE_NODES scripts/anvil/accelerate_1_gpu_bf16_eval.sh configs/qwen3b/libricount/timestamp_any/new_inference/bernoulli[class_weighting][start][bf16][upscale_4][epoch_3][no_schedule][lr_1e-6].yaml outputs/qwen3b/libricount/timestamp_any/new_inference/bernoulli[class_weighting][start][bf16][upscale_4][epoch_3][no_schedule][lr_1e-6]/20251222_014534/checkpoint_epoch3 test
+
+# Audioset
+sbatch $EXCLUDE_NODES scripts/anvil/accelerate_1_gpu_bf16_eval.sh configs/qwen3b/audioset_humans_resampled/timestamp_any/binary[class_weighting][start][bf16][upscale_4][lr_1e-6][epoch_3].yaml outputs/qwen3b/audioset_humans_resampled/timestamp_any/binary[class_weighting][start][bf16][upscale_4][lr_1e-6][epoch_3]/20251222_042343/checkpoint_epoch3 test
+
+# 7B
+# Librispeech
+sbatch $EXCLUDE_NODES scripts/anvil/accelerate_1_gpu_bf16_eval.sh configs/qwen7b/librispeech/timestamp_any/new_inference/bernoulli[class_weighting][start][bf16][upscale_4][epoch_3][no_schedule][lr_1e-6].yaml outputs/qwen7b/librispeech/timestamp_any/new_inference/bernoulli[class_weighting][start][bf16][upscale_4][epoch_3][no_schedule][lr_1e-6]/20251222_042342/checkpoint_epoch3 test
+
+# Libricount
+sbatch $EXCLUDE_NODES scripts/anvil/accelerate_1_gpu_bf16_eval.sh configs/qwen7b/libricount/timestamp_any/new_inference/bernoulli[class_weighting][start][bf16][upscale_4][epoch_6][no_schedule][lr_1e-6].yaml outputs/qwen7b/libricount/timestamp_any/new_inference/bernoulli[class_weighting][start][bf16][upscale_4][epoch_6][no_schedule][lr_1e-6]/20251222_042342/checkpoint_epoch6 test
+
+# Audioset
+sbatch $EXCLUDE_NODES scripts/anvil/accelerate_1_gpu_bf16_eval.sh configs/qwen7b/audioset_humans_resampled/timestamp_any/binary[class_weighting][start][bf16][upscale_4][lr_1e-6][epoch_6].yaml outputs/qwen7b/audioset_humans_resampled/timestamp_any/binary[class_weighting][start][bf16][upscale_4][lr_1e-6][epoch_6]/20251222_054053/checkpoint_epoch3 test
+
+# Ablations
+# < 4
+sbatch $EXCLUDE_NODES scripts/anvil/accelerate_1_gpu_bf16_eval.sh configs/qwen3b/librispeech/timestamp_any/ablation/bernoulli+bidirectional_audio+class_weighting[start][bf16][max_4].yaml outputs/qwen3b/librispeech/timestamp_any/ablation/bernoulli+bidirectional_audio+class_weighting[start][bf16][max_4]/20251223_061132/checkpoint_epoch3 test 4 8 
+
+# < 8
+sbatch $EXCLUDE_NODES scripts/anvil/accelerate_1_gpu_bf16_eval.sh configs/qwen3b/librispeech/timestamp_any/ablation/bernoulli+bidirectional_audio+class_weighting[start][bf16][max_8].yaml outputs/qwen3b/librispeech/timestamp_any/ablation/bernoulli+bidirectional_audio+class_weighting[start][bf16][max_8]/20251223_061142/checkpoint_epoch3 test 8 12
+
+# < 12
+sbatch $EXCLUDE_NODES scripts/anvil/accelerate_1_gpu_bf16_eval.sh configs/qwen3b/librispeech/timestamp_any/ablation/bernoulli+bidirectional_audio+class_weighting[start][bf16][max_12].yaml outputs/qwen3b/librispeech/timestamp_any/ablation/bernoulli+bidirectional_audio+class_weighting[start][bf16][max_12]/20251223_061142/checkpoint_epoch3 test 12 16
+
+# < 16
+sbatch $EXCLUDE_NODES scripts/anvil/accelerate_1_gpu_bf16_eval.sh configs/qwen3b/librispeech/timestamp_any/ablation/bernoulli+bidirectional_audio+class_weighting[start][bf16][max_16].yaml outputs/qwen3b/librispeech/timestamp_any/ablation/bernoulli+bidirectional_audio+class_weighting[start][bf16][max_16]/20251223_084333/checkpoint_epoch2 test 16 20
+
+# < 20
+sbatch $EXCLUDE_NODES scripts/anvil/accelerate_1_gpu_bf16_eval.sh configs/qwen3b/librispeech/timestamp_any/ablation/bernoulli+bidirectional_audio+class_weighting[start][bf16][max_20].yaml outputs/qwen3b/librispeech/timestamp_any/ablation/bernoulli+bidirectional_audio+class_weighting[start][bf16][max_20]/20251223_084347/checkpoint_epoch3 test 20
