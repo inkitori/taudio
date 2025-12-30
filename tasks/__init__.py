d6573bd6b95ddf778d484ddc7455c9c18f761094
inkitori <an.joseph0001@gmail.com>
2025-12-30 05:51:32 -0500

    Removing speaker counting task

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,14 +1,12 @@
 from typing import Dict, Type
 
 from .base_task import BaseTask
-from .speaker_count import SpeakerCountTask
 from .timestamp_all import AllTimestampsTask
 from .timestamp_single_any import SingleTimestampAnyTask
 
 _TASKS: Dict[str, Type[BaseTask]] = {
     "SINGLE_WORD_TIMESTAMP_ANY": SingleTimestampAnyTask,
     "ALL_TIMESTAMPS": AllTimestampsTask,
-    "SPEAKER_COUNT": SpeakerCountTask,
 }
 
 
